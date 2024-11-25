4a9f9c08d933da17b38d7f431701b0a07579123e
Wilifrisquis <hugocruzok@gmail.com>
2024-11-25 19:05:35 +0100

    Segundo Commit Master

---
+++ src/Practica1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Practica1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime un número");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+    }
+}
